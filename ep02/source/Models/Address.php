58fbcc5959030e972e8e6554d7d172a71137dc3d
Artur França <flartur95@gmail.com>
2021-03-23 16:53:13 -0300

    function add

--- ep02/source/Models/Address.php
+++ ep02/source/Models/Address.php
@@ -7,7 +7,19 @@ use CoffeeCode\DataLayer\DataLayer;
 class Adress extends DataLayer{
 
     public function __construct(){
-        parent::__construct ("addresses", [], "", false);
+        parent::__construct ("addresses", ["user_id"], "addr_id", false);
+    }
+
+    public function add (User $user, string $street, string $number): Address{
+        
+        $this->user_id = $user->id;
+        $this->street = $street;
+        $this->number = $number;
+        
+        // $this->save();
+        
+        return $this;
+
     }
     
 }
